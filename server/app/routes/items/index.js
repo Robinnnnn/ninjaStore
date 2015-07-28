fb88cbcad8b41ac5ff7159605190c855f7fcaa71
Robinnnnn <rokim8@gmail.com>
2015-07-28 10:51:42 -0400

    item routes

--- server/app/routes/items/index.js
+++ server/app/routes/items/index.js
@@ -11,14 +11,55 @@ router.get('/', function(req, res, next) {
         .then(function(items) {
             res.json(items)
         })
-        .then(null, next)
+        .then(null, next);
 });
 
-// we could just use filter instead of unique routes
-router.get('/category/:name', function(req, res, next) {
-    Item.getCategory(req.params.name).exec()
-        .then(function(items) {
-            res.json(items)
+router.param('id', function(req, res, next) {
+    Item.findById(req.params.id).then(function(item) {
+        if (item) {
+            req.item = item;
+            next();
+        } else {
+            throw Error('item not found')
+        }
+    }).catch(function(e) {
+        res.status(404).send(e);
+    })
+})
+
+router.get('/:id', function(req, res, next) {
+    res.json(req.item);
+})
+
+router.post('/create', function(req, res, next) {
+    Item.create(req.body)
+        .then(function(item) {
+            req.json(item)
         })
         .then(null, next)
 })
+
+router.put('/edit/:id', function(req, res, next) {
+    for (var key in req.body) {
+        req.item[key] = req.body[key];
+    }
+    req.item.save().then(function(item) {
+        res.json(item);
+    })
+})
+
+router.delete('/delete/:id', function(req, res, next) {
+    req.item.remove()
+        .then(function() {
+            res.status(204).send();
+        })
+})
+
+// // we could just use filter instead of unique routes
+// router.get('/category/:name', function(req, res, next) {
+//     Item.getCategory(req.params.name).exec()
+//         .then(function(items) {
+//             res.json(items)
+//         })
+//         .then(null, next)
+// })
