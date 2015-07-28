cc4fa050fa8dcfc421e40b271f1ab5a7578da043
thang239 <thanghnbk@gmail.com>
2015-07-28 10:04:44 -0400

    Start on homepage

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,14 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        resolve:{
+        	items: function(ItemFactory){
+        		return ItemFactory.getAll();
+        	}
+        },
+        controller:function($scope,items){
+        	$scope.items = items;
+        }
     });
 });
