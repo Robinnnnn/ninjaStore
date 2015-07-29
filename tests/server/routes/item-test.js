ec5d6eb26083a732322c3c694fa5a1b048f045a6
thang239 <thanghnbk@gmail.com>
2015-07-29 11:12:53 -0400

    fixed tests for item routes

--- tests/server/routes/item-test.js
+++ tests/server/routes/item-test.js
@@ -2,6 +2,7 @@
 var mongoose = require('mongoose');
 require('../../../server/db/models');
 var Item = mongoose.model('Item');
+var User = mongoose.model('User');
 
 var expect = require('chai').expect;
 
@@ -39,8 +40,6 @@ describe('Items Route', function() {
 	// });
 
 	describe('Item request', function() {
-
-		var loggedInAgent;
 		var itemId;
 		var itemInfo = {
         name: "Flying Dragon",
@@ -73,65 +72,100 @@ describe('Items Route', function() {
 
 				})
 		});
-		// beforeEach('Create a ite', function(done) {
-		// 	Item.create(itemInfo, done);
-		// });
 
-		beforeEach('Create item agent', function(done) {
-			itemAgent = supertest.agent(app);
-			done();
-			// itemAgent.post('/login').send(userInfo).end(done);
-		});
-
-		it('should get with 200 response and with an array of all items as the body', function(done) {
-			itemAgent.get('/api/items/').expect(200).end(function(err, response) {
-				if (err) return done(err);
-				expect(response.body).to.be.an('array');
-				done();
+		describe('standard user',function(){
+			var userInfo = {
+				email: 'thang@gmail.com',
+				password: '123'
+			};
+		
+			beforeEach('Create a normal user', function (done) {
+				User.create(userInfo, done);
 			});
-		});
-		it('should get single item with 200 response and single item as the body', function(done) {
-			itemAgent.get('/api/items/'+itemId).expect(200).end(function(err, response) {
-				if (err) return done(err);
-				expect(response.body.name).to.be.equal("Flying Dragon");
-				done();
+
+			beforeEach('Create item agent', function(done) {
+				userAgent = supertest.agent(app);
+				userAgent.post('/login').send(userInfo).end(done);
 			});
-		});
 
-		it('should post single item with 201 response and item as body', function(done) {
-			itemAgent.post('/api/items/create')
-					 .send(itemPost)
-					 .expect(201)
-					 .end(function(err, response) {
-						if (err) return done(err);
-						expect(response.body.name).to.be.equal("Dragon");
-						done();
-					  });
-		});
+			it('should get with 200 response and with an array of all items as the body', function(done) {
+				userAgent.get('/api/items/').expect(200).end(function(err, response) {
+					if (err) return done(err);
+					expect(response.body).to.be.an('array');
+					done();
+				});
+			});
+			it('should get single item with 200 response and single item as the body', function(done) {
+				userAgent.get('/api/items/'+itemId).expect(200).end(function(err, response) {
+					if (err) return done(err);
+					expect(response.body.name).to.be.equal("Flying Dragon");
+					done();
+				});
+			});
+			it('should not allow unauthorized user to make change to item', function(done) {
+				userAgent.post('/api/items')
+						 .send(itemPost)
+						 .expect(401)
+						 .end(done);
+			});
+		})
 
-		it('should put item with 200 response and single item as the body', function(done) {
-			itemAgent.put('/api/items/edit/'+itemId)
-					 .send({name:'Flying Dragon Change'})
-					 .expect(200)
-					 .end(function(err, response) {
-						if (err) return done(err);
-						expect(response.body.name).to.be.equal("Flying Dragon Change");
-						done();
-					 });
-		});
+		describe('admin testing',function(){
+			var adminAgent;
 
-		it('should delete item with 204 response and success message as body', function(done) {
-			itemAgent.delete('/api/items/delete/'+itemId)
-					 .expect(204)
-					 .end(function(err, response) {
-						if (err) return done(err);
-						// expect(response).to.be.equal(null);
-						Item.findById(itemId,function(err,item){
-							if(err) return done(err);
-							expect(item).to.be.null;
+			var adminInfo = {
+				email: 'robin@gmail.com',
+				password: '123',
+				isAdmin:true
+			};
+			beforeEach('Create a admin user', function (done) {
+				User.create(adminInfo, done);
+			});
+
+			beforeEach('Create admin user agent and authenticate', function (done) {
+				adminAgent = supertest.agent(app);
+				adminAgent.post('/login').send(adminInfo).end(done);
+			});
+
+			it('should post single item with 201 response and item as body', function(done) {
+				adminAgent.post('/api/items')
+						 .send(itemPost)
+						 .expect(201)
+						 .end(function(err, response) {
+							if (err) return done(err);
+							expect(response.body.name).to.be.equal("Dragon");
 							done();
-						})
-					 });
-		});
+						  });
+			});	
+
+			
+			it('should put item with 200 response and single item as the body', function(done) {
+				adminAgent.put('/api/items/'+itemId)
+						 .send({name:'Flying Dragon Change'})
+						 .expect(200)
+						 .end(function(err, response) {
+							if (err) return done(err);
+							expect(response.body.name).to.be.equal("Flying Dragon Change");
+							done();
+						 });
+			});
+
+			it('should delete item with 204 response and success message as body', function(done) {
+				adminAgent.delete('/api/items/'+itemId)
+						 .expect(204)
+						 .end(function(err, response) {
+							if (err) return done(err);
+							// expect(response).to.be.equal(null);
+							Item.findById(itemId,function(err,item){
+								if(err) return done(err);
+								expect(item).to.be.null;
+								done();
+							})
+						 });
+			});
+
+			
+		})
+
 	});
 });
