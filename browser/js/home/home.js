ffcfbb607d463b37d39557ae92be506a8a2529f1
Robinnnnn <rokim8@gmail.com>
2015-07-29 13:16:00 -0400

    began editing item list view

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,14 +1,27 @@
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html',
-        resolve:{
-        	items: function(ItemFactory){
-        		return ItemFactory.getAll();
-        	}
+        resolve: {
+            items: function(ItemFactory) {
+                return ItemFactory.getAll();
+            }
         },
-        controller:function($scope,items){
-        	$scope.items = items;
+        controller: function($scope, items) {
+            $scope.items = items;
+
+            // makes sure the name of the item doesn't expand its div height
+            function shortenTitle(title, length) {
+                if (title.length > length) {
+                    return title.slice(0, length) + '...';
+                }
+                return title;
+            }
+
+            $scope.items = $scope.items.map(function(item) {
+                item.shortTitle = shortenTitle(item.name, 35);
+                return item;
+            })
         }
     });
 });
