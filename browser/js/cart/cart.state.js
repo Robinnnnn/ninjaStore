3c6e0aefcd1eb36d7ec841b74bbcb9f4bf8cd02b
Will Ruggiano <will@ruggianofamily.com>
2015-07-29 12:54:55 -0400

    added order factory and cart state

--- browser/js/cart/cart.state.js
+++ browser/js/cart/cart.state.js
@@ -1,7 +1,12 @@
-app.config(function($stateParams) {
-  $stateParams.state('cart', {
+app.config(function($stateProvider) {
+  $stateProvider.state('cart', {
     url: '/cart',
     templateUrl: '/js/cart/cart.html',
-    resolve: ''
+    resolve: {
+      order: (Order) => Order.fetchCurrent()
+    },
+    controller: ($scope, order) => {
+      $scope.order = order
+    }
   })
 })
---
+++ browser/js/common/factories/order.factory.js
@@ -0,0 +1,51 @@
+app.factory('Order', function($http) {
+  function Order(props) {
+    angular.extend(this, props)
+    return this
+  }
+
+  Order.url = '/api/orders/'
+
+  Object.defineProperty(Order.prototype, 'url', {
+    get: () => Order.url + this._id
+  })
+
+  Order.isNew = () => !this._id
+
+  Order.fetchAll = () => {
+    return $http.get(Order.url)
+      .then(res => {
+        return res.data.map(obj => {
+          return new Order(obj)
+        })
+      })
+  }
+
+  Order.fetchCurrent = () => {
+    return $http.get(Order.url + 'current')
+      .then(res => new Order(res.data))
+  }
+
+  Order.prototype.fetch = () => {
+    return $http.get(this.url)
+      .then(res => new Order(res.data))
+  }
+
+  Order.prototype.save = () => {
+    let verb
+    let url
+    if (this.isNew()) {
+      verb = 'post'
+      url = Order.url
+    } else {
+      verb = 'put'
+      url = this.url
+    }
+    return $http[verb](url, this)
+      .then(res => new Order(res.data))
+  }
+
+  Order.prototype.destroy = () => $http.delete(this.url)
+
+  return Order
+})
