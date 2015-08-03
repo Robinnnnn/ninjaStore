0415b00f5967829cf1a10bdffea91ff640a2d973
Will Ruggiano <will@ruggianofamily.com>
2015-08-03 14:58:36 -0400

    fixed

---
+++ browser/js/common/directives/addReview/addReview.directive.js
@@ -0,0 +1,25 @@
+app.directive('addReview', function(Review) {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/common/directives/addReview/addReview.html',
+    scope: {
+      item: '='
+    },
+    link: (scope, elem, attr) => {
+      // scope.newReview = {}
+      scope.submitReview = () => {
+        let toSave = new Review(scope.newReview)
+        toSave.itemId = scope.item._id
+        console.log(toSave)
+        toSave.save()
+          .then(review => {
+            scope.item.reviews.push(review)
+            return scope.item.save()
+          })
+          .then(() => {
+            console.log('review saved and item updated');
+          })
+      }
+    }
+  }
+})
