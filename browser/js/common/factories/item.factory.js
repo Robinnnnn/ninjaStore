5d453650ae740a346b7e16019584dbbd28bd472f
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-03 15:57:41 -0400

    fixed merge conflicts

--- browser/js/common/factories/item.factory.js
+++ browser/js/common/factories/item.factory.js
@@ -21,7 +21,6 @@ app.factory('Item', function($http) {
 			.then(res => {
 				return res.data.map(obj => new Item(obj))
 			})
-		})
 	}
 
 	Item.getCategories = function() {
@@ -33,11 +32,7 @@ app.factory('Item', function($http) {
 
 	Item.prototype.fetch = function() {
 		return $http.get(this.url)
-<<<<<<< HEAD
-		.then(res => new Item(res.data))
-=======
 			.then(res => new Item(res.data))
->>>>>>> master
 	}
 
 	Item.prototype.addToCart = function() {
@@ -59,30 +54,9 @@ app.factory('Item', function($http) {
 		.then(res => new Item(res.data))
 	}
 
-<<<<<<< HEAD
-	Item.prototype.destroy = function() { return $http.delete(this.url) }
-
-	return Item
-})
-=======
-	Item.prototype.save = function() {
-		let verb
-		let url
-		if (this.isNew()) {
-			verb = 'post'
-			url = Item.url
-		} else {
-			verb = 'put'
-			url = this.url
-		}
-		return $http[verb](url, this)
-			.then(res => new Item(res.data))
-	}
-
 	Item.prototype.destroy = function() {
 		return $http.delete(this.url)
 	}
 
 	return Item
 })
->>>>>>> master
