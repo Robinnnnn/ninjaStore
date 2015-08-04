56d3ba5bf683f7f75998108d4b382a26d71c4e6b
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-04 16:41:04 -0400

    pagination bar on home page

--- browser/js/home/main.controller.js
+++ browser/js/home/main.controller.js
@@ -1,6 +1,37 @@
-app.controller('MainCtrl', ($scope, items, $log) => {
-    $scope.activeCategory = ''
-    $scope.items = items;
+app.controller('MainCtrl', ($scope, items) => {
+    $scope.activeCategory = '';
+
+
+    var itemsPerPage = 12;
+    $scope.numItems = items.length;
+    $scope.numPages = Math.ceil($scope.numItems/itemsPerPage);
+    $scope.pages = [];
+    for(var i = 1; i<=$scope.numPages; i++){
+        $scope.pages[i-1] = i;
+    }
+
+    var itemsArr = items.reduce(function(acc, elem, index){
+        if(index%itemsPerPage === 0) acc.push([]);
+        acc[acc.length-1].push(elem);
+        return acc;
+    }, []);
+
+    $scope.itemsArr = itemsArr;
+
+
+    $scope.activePage = 1;
+
+    $scope.onPage = function(page){
+        if(page === $scope.activePage) return true;
+        else return false;
+    };
+
+    $scope.currentPage = function(page){
+        console.log("Current page", page)
+        $scope.activePage = page;
+    };
+
+    $scope.items = items.slice(0,itemsPerPage);
 
     // makes sure the name of the item doesn't expand its div height
     function shortenTitle(title, length) {
@@ -16,18 +47,18 @@ app.controller('MainCtrl', ($scope, items, $log) => {
     })
 
 
-    $scope.totalItems = items.length;
-    $scope.currentPage = 1;
+    // $scope.totalItems = items.length;
+    // $scope.currentPage = 1;
 
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
+    // $scope.setPage = function (pageNo) {
+    //     $scope.currentPage = pageNo;
+    // };
 
-    $scope.pageChanged = function() {
-        $log.log('Page changed to: ' + $scope.currentPage);
-    };
+    // $scope.pageChanged = function() {
+    //     $log.log('Page changed to: ' + $scope.currentPage);
+    // };
 
-    $scope.maxSize = 8;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
+    // $scope.maxSize = 8;
+    // $scope.bigTotalItems = 175;
+    // $scope.bigCurrentPage = 1;
 })
