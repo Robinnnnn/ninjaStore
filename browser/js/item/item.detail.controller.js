a4bf1025267b15e328a2c4fedb70536d5b6c1c65
Robinnnnn <rokim8@gmail.com>
2015-08-04 16:42:53 -0400

    more contenteditable stuff

--- browser/js/item/item.detail.controller.js
+++ browser/js/item/item.detail.controller.js
@@ -3,17 +3,17 @@ app.controller('itemDetailCtrl', function($scope, item, recommendations, user, I
 	$scope.recommendations = recommendations
 	$scope.user = user;
 	$scope.admin = user.isAdmin;
-	$scope.editOrSave = "edit"
+	$scope.editOrSave = "Edit"
 
 	$scope.editOrSaveToggle = function(item) {
-		if ($scope.editOrSave === "edit") {
-			$scope.editOrSave = "save"
+		if ($scope.editOrSave === "Edit") {
+			$scope.editOrSave = "Save"
 		} else {
 			$scope.item.name = $scope.item.name.trim()
 			$scope.item.quantity = Number($scope.item.quantity)
 			$scope.item.save().then(function(item) {
 				console.log(item)
-				$scope.editOrSave = "edit"
+				$scope.editOrSave = "Edit"
 			})
 		}
 	}
