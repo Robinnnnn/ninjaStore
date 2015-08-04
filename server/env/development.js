3a9dda6b655ab53c3a5e3ed24cafc3dfd27c65ec
Will Ruggiano <will@ruggianofamily.com>
2015-08-04 11:07:03 -0400

    google, facebook added

--- server/env/development.js
+++ server/env/development.js
@@ -9,11 +9,11 @@ module.exports = {
   "FACEBOOK": {
     "clientID": "1476011056044369",
     "clientSecret": "191f6c8d0f32f584b96e1a562eb8dd83",
-    "callbackURL": "http://127.0.0.1:1337/"
+    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "853502665892-bu9golih10gctphqoriueivhgjc5fthg.apps.googleusercontent.com",
     "clientSecret": "JtCswpqmsSCMjxbP004wikPb",
-    "callbackURL": "http://localhost:1337/oauth2callback"
+    "callbackURL": "http://localhost:1337/auth/google/callback"
   }
 };
