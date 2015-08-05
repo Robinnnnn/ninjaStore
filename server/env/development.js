e614bde4160f0bf1f4d3151065b250a99e34e23b
thang239 <thanghnbk@gmail.com>
2015-08-05 09:52:17 -0400

    heroku

--- server/env/development.js
+++ server/env/development.js
@@ -9,10 +9,10 @@ module.exports = {
   "FACEBOOK": {
     "clientID": "1476011056044369",
     "clientSecret": "191f6c8d0f32f584b96e1a562eb8dd83",
-    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
+    "callbackURL": "https://boiling-gorge-2488.herokuapp.com/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "853502665892-bu9golih10gctphqoriueivhgjc5fthg.apps.googleusercontent.com",
     "clientSecret": "JtCswpqmsSCMjxbP004wikPb",
-    "callbackURL": "http://localhost:1337/auth/google/callback"
+    "callbackURL": "https://boiling-gorge-2488.herokuapp.com/auth/google/callback"
 }};
